b76705c8a34d10ffdf1615288a621544195a73c6
Aurobindo Naik <aurobindo.naik@gmail.com>
2016-10-14 22:59:15 +0100

    VPRC Project Initialized

---
+++ src/main/java/org/jw/vprc/VprcApplication.java
@@ -0,0 +1,12 @@
+package org.jw.vprc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VprcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VprcApplication.class, args);
+	}
+}
---
+++ src/test/java/org/jw/vprc/VprcApplicationTests.java
@@ -0,0 +1,16 @@
+package org.jw.vprc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VprcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
